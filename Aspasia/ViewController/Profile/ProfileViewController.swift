510988afb51036d3fa9e74229841e5a4b48963da
이동근 <eastroot@idong-geun-ui-MacBookAir.local>
2021-08-11 22:11:59 +0900

    StackERView 추가

--- Aspasia/ViewController/Profile/ProfileViewController.swift
+++ Aspasia/ViewController/Profile/ProfileViewController.swift
@@ -6,30 +6,36 @@
 //
 
 import UIKit
+import StackERView
 
 class ProfileViewController: UIViewController {
-    var scrollView: VStackScroll!
-
+    let scrollView = VStackERScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         
-        scrollView = VStackScroll(frame: view.frame)
-        scrollView.alignment = .left
+        scrollView.stackAlignment = .left
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
-        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+
         let banner = UIView()
         banner.backgroundColor = .systemRed
         scrollView.setBanner(banner, height: 400)
-        
-        for _ in 0..<10 {
+
+        for _ in 0..<15 {
             let label = UILabel()
             label.textColor = .black
             label.font = .systemFont(ofSize: 24)
             label.text = "프로필 정보가 나오는 화면입니다."
-            
+
             scrollView.push(label, spacing: 10)
         }
     }
